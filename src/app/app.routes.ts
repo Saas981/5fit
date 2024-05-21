4934a95b3d93021bd64e7767c4bfda42f77b8d8d
YarnGrowler <yarngrowler@gmail.com>
2024-05-21 16:59:41 -0500

    Nextjs commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { SettingsComponent } from './settings/settings.component';
-import { DisplayComponent } from './display/display.component';
-
-export const routes: Routes = [
-    { path: '', component: SettingsComponent },
-    { path: 'display', component: DisplayComponent },
-];
